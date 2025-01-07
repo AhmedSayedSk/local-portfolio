0835706ac6761762c1ba341152dd61686045a4a2
Ahmed Sayed Sk <ahmedsk128@gmail.com>
2025-01-07 21:59:12 +0200

    Install Quasar framework and do example in HelloWorld component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import { Quasar } from 'quasar'
+
 export default {
   name: 'HelloWorld',
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HelloWorld from '../components/HelloWorld.vue';
+import { Quasar } from 'quasar';
 
 const routes = [
   {
